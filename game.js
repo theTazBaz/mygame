aa53d141e291d54fcb46029ff444af8251acf8b2
theTazBaz <150133311+theTazBaz@users.noreply.github.com>
2024-11-08 02:44:59 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var config = {
+      width: 800,
+      height: 800,
+      scene: [Scene1, Scene2],
+      backgroundColor: 0x000000,
+    };
+
+    var game = new Phaser.Game(config);
+  };
